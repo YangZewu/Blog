2ffa547b7e939c04d6ab36c29c78d1cd4724276f
Mr_Yang <1512743313@qq.com>
2019-05-11 21:21:20 +0800

    添加项目文件。

---
+++ Blog.BLL/userServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.DAL;
+using Blog.Model;
+namespace Blog.BLL
+{
+   public class userServer
+    {
+        private userDal userdal = new userDal();
+        //查询
+        public List<T_User> GetLists(Expression<Func<T_User, bool>> whereLambda)
+        {
+            return userdal.GetT_Users(whereLambda);
+        }
+        //增加
+        public bool Add(T_User u)
+        {
+            return userdal.Add(u);
+        }
+        //更新
+        public bool Updata(T_User u)
+        {
+            return userdal.Updata(u);
+        }
+        //删除
+        public bool Delete(int id)
+        {
+            return userdal.Delete(id);
+        }
+    }
+}
---
+++ Blog.DAL/userDal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Model;
+using System.Linq.Expressions;
+namespace Blog.DAL
+{
+   public class userDal
+    {
+        private BloggerEntities user = new BloggerEntities();
+        //查询
+        public List<T_User> GetT_Users(Expression<Func<T_User, bool>> whereLambda)
+        {
+            return user.T_User.Where(whereLambda).ToList();
+        }
+        //增加
+        public bool Add(T_User u)
+        {
+            user.T_User.Add(u);
+            return user.SaveChanges() > 0;
+        }
+        //更新
+        public bool Updata(T_User u)
+        {
+            var us = user.T_User.Where(a=>a.UserName==u.UserName).FirstOrDefault();
+            us.UserPassword = u.UserPassword;
+            return user.SaveChanges() > 0;
+        }
+        //删除
+        public bool Delete(int id)
+        {
+            var us = new T_User { UserId = id };
+            user.T_User.Attach(us);
+            user.T_User.Remove(us);
+            return user.SaveChanges() > 0;
+        }
+    }
+}
