0f509302480dee76372aee64cc69222df5134b5c
Mr_Yang <1512743313@qq.com>
2019-05-11 23:15:26 +0800

    1.0.1

--- Blog.UI/Controllers/UserController.cs
+++ Blog.UI/Controllers/UserController.cs
@@ -3,19 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Blog.Model;
+using Blog.BLL;
 namespace Blog.UI.Controllers
 {
     public class UserController : Controller
     {
+        private userServer user = new userServer();
         // GET: User
         public ActionResult Index()
         {
             return View();
         }
+        //注册页
         public ActionResult Reg()
         {
             return View();
         }
+        //注册方法
+        public JsonResult Add(T_User u)
+        {
+            u.RegTime = System.DateTime.Now.ToString();
+            u.UserState = "1";
+            if (user.Add(u))
+            {
+                return Json(new { msg = "添加成功", success = true });
+            }
+            else
+            {
+                return Json(new { msg = "添加失败", success = false });
+            }
+        }
+        //登录页
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //登录方法
+        public JsonResult GetUser(T_User u)
+        {
+            if (user.GetLists(a => a.UserName == u.UserName && a.UserPassword == u.UserPassword).Count >= 1)
+            {
+                return Json(new { msg = "登录成功", success = true });
+            }
+            else
+            {
+                return Json(new { msg = "登录失败", success = false });
+            }
+        }
+        //找回方法
+        public JsonResult Edit(T_User u)
+        {
+            if(user.Updata(u))
+            {
+                return Json(new { msg = "找回成功", success = true });
+            }
+            else
+            {
+                return Json(new { msg = "找回失败", success = false });
+            }
+        }
+        
     }
 }
---
+++ Blog.UI/Scripts/user.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+    //找回
+    $("#btnEdit").click(function () {
+        $.ajax({
+            url: "/User/Edit",
+            type: "Post",
+            data: {
+                "UserName": $("#inputEUName").val(),
+                "UserPassword": $("#inputEPwd").val()
+            },
+            success: function (data) {
+                if (data.success) {
+                    alert("找回成功");
+                    $("#myModal").modal("hide");
+                }
+                else {
+                    alert("找回失败");
+                }
+            }
+        });
+    }),
+    //弹出窗口
+    $("#btnUp").click(function () {
+        $("#myModal").modal();
+    }),
+    //登录
+    $("#btnLog").click(function () {
+        $.ajax({
+            url: "/User/GetUser",
+            type: "Post",
+            data: {
+                "UserName": $("#inputUName").val(),
+                "UserPassword": $("#inputUPwd").val()
+            },
+            success: function (data) {
+                if (data.success) {
+                    alert("登录成功");
+                }
+                else {
+                    alert("登录失败");
+                }
+            }
+        });
+    }),
+    //注册
+    $("#btnReg").click(function () {
+        $.ajax({
+            url: "/User/Add",
+            type: "Post",
+            data: {
+                "UserName": $("#inputUName").val(),
+                "UserPet": $("#inputUPet").val(),
+                "UserPassword": $("#inputUPwd").val(),
+                "UserSex": $("#USex").val(),
+                "UserBirthday": $("#inputUBr").val(),
+                "UserBirthplace": $("#inputUAdders").val(),
+                "UserMailbox": $("#inputEmail").val(),
+                "UserQQ": $("#inputQQ").val(),
+                "UserSig": $("#inputUserSig").val()
+            },
+            success: function (data) {
+                if (data.success) {
+                    alert("注册成功");
+                    window.location = "/User/Login";
+                }
+                else {
+                    alert("注册失败");
+                }
+            }
+        });
+    });
+});
